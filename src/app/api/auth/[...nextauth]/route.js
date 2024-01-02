ac469d34a1d3c689b6f51b4354d4a6bb9bdab479
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-02 14:09:19 +0100

    Fix problem

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
